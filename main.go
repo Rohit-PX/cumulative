84c5de820701632346d2fbb583129803ad8e8a4f
Rohit-PX <rkulkarni@purestorage.com>
2022-07-27 00:17:13 +0000

    Add param validation
    Signed-off-by: Rohit-PX <rkulkarni@purestorage.com>

--- main.go
+++ main.go
@@ -23,7 +23,19 @@ type Events []*Event
 
 type EventType string
 
-func GetEventsFromFile(fileName string) (*Events, error) {
+var eventsDB *Events
+
+// Implement delimiter as a variable it could be "," or "|"
+const (
+	VEST   EventType = "VEST"
+	CANCEL EventType = "CANCEL"
+)
+
+func Init() *Events {
+	return &Events{}
+}
+
+func GetVestingFromFile(fileName string) (*Events, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to open file %s, error: %v", fileName, err)
@@ -31,10 +43,10 @@ func GetEventsFromFile(fileName string) (*Events, error) {
 
 	records, err := csv.NewReader(file).ReadAll()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read file %s, error: . ")
+		return nil, fmt.Errorf("Failed to read CSV file %s, error: %v", fileName, err)
 	}
 
-	eventsDB := Init()
+	eventsDB = Init()
 
 	for _, event := range records {
 		e := &Event{
@@ -42,9 +54,12 @@ func GetEventsFromFile(fileName string) (*Events, error) {
 			EmployeeID: event[1],
 			Name:       event[2],
 			AwardID:    event[3],
-			EventDate:  ConvertToDate(event[4]),
 		}
-		// Add to code validation part
+		eventDate, err := ConvertToDate(event[4])
+		if err != nil {
+			return nil, err
+		}
+		e.EventDate = eventDate
 		quantity, err := strconv.ParseFloat(event[5], 2)
 		if err != nil {
 			return nil, err
@@ -57,51 +72,108 @@ func GetEventsFromFile(fileName string) (*Events, error) {
 
 }
 
-// Implement delimiter as a variable it could be "," or "|"
-const (
-	VEST   EventType = "VEST"
-	CANCEL EventType = "CANCEL"
-)
-
-func Init() *Events {
-	return &Events{}
-}
-
-func PrintEvent(e *Event) {
-	fmt.Printf("%s %s %s %s %s %f\n", e.Type, e.EmployeeID, e.Name, e.AwardID, (e.EventDate).Format("2006-01-02"), e.Quantity)
-}
-
-func PrintEvents(events *Events) {
+func GetVestingBefore(t time.Time, events *Events, precision int) Events {
+	vestingBefore := Init()
 	for _, e := range *events {
-		PrintEvent(e)
+		if !(e.EventDate.Before(t) || e.EventDate == t) {
+			e.Quantity = 0
+		}
+		*vestingBefore = append(*vestingBefore, e)
 	}
+	return *vestingBefore
 }
 
-func GetEventsBefore(t time.Time, events *Events) {
-	for _, e := range *events {
-		if e.EventDate.Before(t) || e.EventDate == t {
-			PrintEvent(e)
+func GetVestingSchedule(events Events, precision int) {
+	vestingMap := make(map[string]*Event)
+	for _, e := range events {
+		key := fmt.Sprintf("%s-%s", e.EmployeeID, e.AwardID)
+		if current, ok := vestingMap[key]; ok {
+			if e.Type == VEST {
+				current.Quantity = current.Quantity + e.Quantity
+			} else if e.Type == CANCEL {
+				current.Quantity = current.Quantity - e.Quantity
+			}
+		} else {
+			// Key not found
+			vestingMap[key] = &Event{
+				EmployeeID: e.EmployeeID,
+				Name:       e.Name,
+				AwardID:    e.AwardID,
+				Quantity:   e.Quantity,
+			}
 		}
 	}
+
+	for _, event := range vestingMap {
+		PrintEvent(event, precision)
+	}
 }
 
-func ConvertToDate(str string) time.Time {
+/*
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+*/
+
+func ValidateParams(fileParam, dateParam, precisionParam string) (string, time.Time, int, error) {
+	_, err := os.Open(fileParam)
+	if err != nil {
+		return "", time.Time{}, 0, fmt.Errorf("Failed to open file %s, error: %v. Check if file exists or file name or path to file is incorrect", fileParam, err)
+	}
+
+	targetDate, err := ConvertToDate(dateParam)
+	if err != nil {
+		return "", time.Time{}, 0, err
+	}
+
+	precision, err := strconv.Atoi(precisionParam)
+	if err != nil || precision < 0 {
+		return "", time.Time{}, 0, fmt.Errorf("Incorrect precision param  %s. Error: %v", precisionParam, err)
+	}
+	return fileParam, targetDate, precision, nil
+}
+
+func ConvertToDate(str string) (time.Time, error) {
 	layout := "2006-01-02"
 	t, err := time.Parse(layout, str)
 	if err != nil {
-		log.Fatal(err)
+		return time.Time{}, fmt.Errorf("Invalid date format. Error: %v. Please pass date in the following format - YYYY-MM-DD")
 	}
-	return t
+	return t, nil
+}
+
+func PrintEvent(e *Event, precision int) {
+	fmt.Printf("%s %s %s %s %s %s\n", e.Type, e.EmployeeID, e.Name, e.AwardID, (e.EventDate).Format("2006-01-02"), strconv.FormatFloat(e.Quantity, 'f', precision, 64))
+}
+
+func PrintEvents(events *Events, precision int) {
+	for _, e := range *events {
+		PrintEvent(e, precision)
+	}
 }
 
 func main() {
-	fileName := os.Args[1]
-	targetDate := ConvertToDate(os.Args[2])
-	allEvents, err := GetEventsFromFile(fileName)
+
+	// TODO add validation of input params
+	fileName, targetDate, precision, err := ValidateParams(os.Args[1], os.Args[2], os.Args[3])
 	if err != nil {
 		log.Fatal(err)
 	}
-	PrintEvents(allEvents)
-	fmt.Println("------------------------------------")
-	GetEventsBefore(targetDate, allEvents)
+
+	// TODO add error classes
+	// TODO move methods to controllers/packages
+	allEvents, err := GetVestingFromFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(1)
+	//PrintEvents(eventsDB, precision)
+	filteredEvents := GetVestingBefore(targetDate, allEvents, precision)
+	GetVestingSchedule(filteredEvents, precision)
 }
